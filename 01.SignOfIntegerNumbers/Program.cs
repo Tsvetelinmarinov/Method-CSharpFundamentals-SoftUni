a1aebba9474e6bfc499b1558609fb5c983a01168
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-07 14:25:17 +0300

    Add project files.

---
+++ 01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * 01.SignOfIntegerNumbers
+ */
+
+using System;
+
+namespace _01.SignOfIntegerNumbers;
+
+internal class Program
+{
+
+    static void Main()
+    {
+
+        /**
+         * A single integer is given, create a method that checks if the given number is positive, 
+         *  negative, or zero. As a result print:
+         *  
+         *  •	For positive number: "The number {number} is positive. "
+         *  •	For negative number: "The number {number} is negative. "
+         *  •	For zero number: "The number {number} is zero. "
+         *
+         */
+
+        int inputStream = int.Parse(Console.ReadLine() ?? "666");
+        CheckNumberStatus(number: inputStream);
+
+    }
+
+    /// <summary>
+    ///  Checks if the number is positive, negative or zero and prints the result
+    /// </summary>
+    /// <param name="number">The number to be checked</param>
+    static void CheckNumberStatus(int number = 666)
+    {
+
+        string? status = default;
+        if (number == 0)
+        {
+            status = $"The number {number} is zero.";
+        }
+        else if (number < 0)
+        {
+            status = $"The number {number} is negative.";
+        }
+        else if (number > 0)
+        {
+            status = $"The number {number} is positive.";
+        }
+        else
+        {
+            status = $"Error";
+        }
+
+        Console.WriteLine(status);
+
+    }
+
+}
---
+++ 02.Grades/Program.cs
@@ -0,0 +1,62 @@
+﻿/**
+ * 02. Grades
+ */
+
+namespace _02.Grades;
+
+internal class Program
+{
+
+    static void Main()
+    {
+
+        //Create a method that receives a grade between 2.00 and 6.00 and prints the
+        // corresponding grade definition:
+        //•	2.00 – 2.99 - "Fail"
+        //•	3.00 – 3.49 - "Poor"
+        //•	3.50 – 4.49 - "Good"
+        //•	4.50 – 5.49 - "Very good"
+        //•	5.50 – 6.00 - "Excellent"
+
+        float inputGrade = float.Parse(Console.ReadLine() ?? "666f");
+        Console.WriteLine(GenerateAssessment(inputGrade));
+
+    }
+
+    /// <summary>
+    /// Generates a string representations of a grade
+    /// </summary>
+    /// <param name="grade">The grade(integer) to be converted</param>
+    /// <returns></returns>
+    static string GenerateAssessment(float grade = 6f)
+    {
+        string? assessment = default;
+        if (grade >= 2 && grade <= 2.99)
+        {
+            assessment = "Fail";
+        }
+        else if (grade >= 3 && grade <= 3.49)
+        {
+            assessment = "Poor";
+        }
+        else if (grade >= 3.5 && grade <= 4.49)
+        {
+            assessment = "Good";
+        }
+        else if (grade >= 4.5 && grade <= 5.49)
+        {
+            assessment = "Very good";
+        }
+        else if (grade >= 5.5 && grade <= 6)
+        {
+            assessment = "Excellent";
+        }
+        else
+        {
+            assessment = "Error";
+        }
+
+        return assessment;
+    }
+
+}
