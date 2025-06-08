e9e3993ac93af4302f97e8306e42185ecc07258c
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-08 20:41:29 +0300

    Задача 6

---
+++ 06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * 06.CalculateRectangleArea
+ */
+
+namespace _06.CalculateRectangleArea;
+
+class Program
+{
+    static void Main()
+    {
+        int sideA = int.Parse(Console.ReadLine()!);
+        int sideB = int.Parse(Console.ReadLine()!);
+        double area = GetArea(a: sideA, b: sideB);
+        Console.WriteLine(area);
+    }
+
+    static double GetArea(int a = 6, int b = 6)
+    {
+        return a * b;
+    }
+}
