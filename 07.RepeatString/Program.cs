8e14fa93327d1cdbd32df7fca91243792fc9ec4c
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-11 06:59:20 +0300

    Задачи 7 и 8

---
+++ 07.RepeatString/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * 07.RepeatString
+ */
+
+namespace _07.RepeatString;
+
+class Program
+{
+    static void Main()
+    {
+        string inputStream = Console.ReadLine() ?? "6";
+        int count = int.Parse(Console.ReadLine() ?? "6");
+        string fullString = Repeat(text: inputStream, count: count);
+
+        Console.WriteLine(fullString);
+    }
+
+    static string Repeat(string text, int count = 6)
+    {
+        string repeated = default!; 
+        for (int i = 0; i < count; i++)
+        {
+            repeated = String.Concat(text, repeated);
+        }
+
+        return repeated;
+    }
+}
---
+++ 08.MathPower/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+/**
+ * 08.MathPower
+ */
+namespace _08.MathPower;
+
+class Program
+{
+    static void Main()
+    {
+        double baseNumber = double.Parse(Console.ReadLine()!);
+        double powerNumber = double.Parse(Console.ReadLine()!);
+        double power = GetPower(_base: baseNumber, power: powerNumber);
+        Console.WriteLine(power);
+    }
+
+    static double GetPower(double _base = 6, double power = 6) 
+    {
+        double buffer = _base;
+        for (int i = 0; i < power - 1; ++i)
+        {
+            _base *= buffer;
+        }
+
+        return _base;
+    } 
+}
