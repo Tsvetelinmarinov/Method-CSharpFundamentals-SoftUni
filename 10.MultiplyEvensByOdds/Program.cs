63816a894897817d521e6d1744a2ad89126d6005
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-06-11 23:14:48 +0300

    Задачи 10 и 11

---
+++ 10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * 10.MultiplyEvensByOdds
+ */
+
+namespace _10.MultiplyEvensByOdds;
+
+class Program
+{
+    static void Main()
+    {
+        int input = Math.Abs(int.Parse(Console.ReadLine()!));
+        int total = SplitEvents(number: input) * SplitOdss(number: input);
+        Console.WriteLine(total);
+    }
+
+    static int SplitEvents(int number = 666)
+    {
+        int sum = default;
+        while (number > 0)
+        {
+            int currentDigit = number % 10;
+            number /= 10;
+            if (currentDigit % 2 == 0)
+            {
+                sum += currentDigit;
+            }
+        }
+
+        return sum;
+    }
+
+    static int SplitOdss(int number = 666)
+    {
+        int sum = default;
+        while (number > 0)
+        {
+            int currentDigit = number % 10;
+            number /= 10;
+            if (currentDigit % 2 == 1)
+            {
+                sum += currentDigit;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ 11.MathOperations/Program.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * 11.MathOperations
+ */
+
+namespace _11.MathOperations;
+
+class Program
+{
+    static void Main()
+    {
+        double leftValue = double.Parse(Console.ReadLine()!);
+        char action = char.Parse(Console.ReadLine()!);
+        double rigthValue = double.Parse(Console.ReadLine()!);
+        double result = Calculate(leftSide: leftValue, rigthSide: rigthValue, operation: action);
+
+        Console.WriteLine(result);
+    }
+
+    static double Calculate(double leftSide = 666d, double rigthSide = 666d, char operation = '*')
+    {
+        return operation switch
+        {
+            '*' => leftSide * rigthSide,
+            '/' => leftSide / rigthSide,
+            '+' => leftSide + rigthSide,
+            '-' => leftSide - rigthSide,
+            _ => 6
+        };
+    }
+}
